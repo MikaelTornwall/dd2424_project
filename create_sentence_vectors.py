84e6f38159667c1b43db7f8670a4911f5aa58a6b
Filippa Karrfelt <filippakarrfelt@Filippas-MacBook-Air-2.local>
2021-05-13 11:23:27 +0200

    Start to fix the common vectors for all sentences

--- create_sentence_vectors.py
+++ create_sentence_vectors.py
@@ -19,7 +19,7 @@ def find_common_words(doc, n):
     X = X.toarray()
     X = np.array(X)
     # TODO: fix this in the parsing step??
-    # check so that the document is long enough!
+    # check so that the document is long enough
     if X.shape[0] < 1 or X.shape[1] < n:
         return False, []
 
@@ -41,25 +41,47 @@ def compute_tf_for_docs(n_features):
     # read and parse the data
     bc3_df = pd.read_pickle(BC3_PICKLE_LOC)
     docs = bc3_df['tokenized_body'].tolist()
-    
+    print('docs 0: ', docs[2])
     # data structure to hold the term frequencies for each document (the key is document row in the panda)
-    doc_tfs = {}
+    doc_tfs = []
 
     for idx, d in enumerate(docs):
         # Create the vocabulary for each document that is of common length across documents
         include, vocabulary = find_common_words(d, n_features)
         # if the document has enough words to be used (more than the wanted number of features)
         if include:
-            vectorizer =  TfidfVectorizer(vocabulary=vocabulary, use_idf=False)
+            vectorizer =  TfidfVectorizer(vocabulary=vocabulary)
             X = vectorizer.fit_transform(d)
             X = np.array(X.toarray())
-
             # Adding small values to the feature vectors
             scores = np.random.normal(0, 1e-8, (X.shape))
             scores = scores + X
-            doc_tfs[idx] = scores
+            doc_tfs.append({"id": idx, "tf": scores })
+    
+    # TODO: return in pandas format
+    return pd.DataFrame(doc_tfs)
 
-    return doc_tfs
+"""
+Function that creates the tf_idf vector representations for each of the sentences in the corpus.
+TODO: fix this! the fit_transform is not working properly! 
+Also need to figure out a way to connect the final vectors to the corresponding document.
+"""
+def tf_idf_vecs_for_all():
+    bc3_df = pd.read_pickle(BC3_PICKLE_LOC)
+    docs = bc3_df['tokenized_body']
+    # TODO: how to keep track of all sentences??
+    all_sents = []
+    for d in docs:
+        for sent in d:
+            all_sents.append(sent)
+    all_sents = np.array(all_sents, dtype=object)
+    v =  TfidfVectorizer()
+    tfidf = v.fit_transform(all_sents)
+    return pd.DataFrame(tfidf)
 
 n_features = 10
-tfs_for_docs = compute_tf_for_docs(n_features)
+doc_tf = compute_tf_for_docs(n_features)
+print(doc_tf)
+
+corpus_tf_idf = tf_idf_vecs_for_all()
+print(corpus_tf_idf)
