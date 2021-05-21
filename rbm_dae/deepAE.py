f14d3593c9eb211fb1a065ffb79830703d4df435
Isak <isakpet@kth.se>
2021-05-21 11:42:57 +0200

    updated deepAE

--- rbm_dae/deepAE.py
+++ rbm_dae/deepAE.py
@@ -43,11 +43,10 @@ def stackedRBM(sentences, sentenceloader, train_batch_size):
     current_sentenceloader = sentenceloader
 
     for hidden_dim in [140, 40, 30, 10]:
-        num_epochs = 30 if hidden_dim == 10 else 10  # Less epochs for last layer, maybe remove?
-        learning_rate = 1e-3 if hidden_dim == 10 else 0.1  # Changing the learning rate for last layer , maybe remove?
+        num_epochs = 50
+        learning_rate = 1e-3
         use_gaussian = hidden_dim == 10  # Use gaussian distribution on last layer
 
-        # print("Layer: ", visible_dim)
 
         rbm = RBM(visible_dim=visible_dim, hidden_dim=hidden_dim,
                   gaussian_hidden_distribution=use_gaussian)
@@ -63,7 +62,7 @@ def stackedRBM(sentences, sentenceloader, train_batch_size):
 
                 # Not sure if we should keep momentum coefficient and weigh_decay?
                 rbm.update_weights(v0, pvk, rbm.sample_h(v0)[0], rbm.sample_h(pvk)[0], learning_rate,
-                                   momentum_coef=0.5 if epoch < 5 else 0.9,         # Remove ?
+                                   momentum_coef=0.5 if epoch < 5 else 0.9,
                                    weight_decay=2e-4,
                                    batch_size=sample_data.shape[0])
 
@@ -102,7 +101,7 @@ def train_DAE(sentence_vectors, batch_size):
     rbm_models = stackedRBM(sentence_vectors, sentenceloader, train_batch_size)
 
     # fine-tune Deep Auto Encoder
-    dae = DAE(rbm_models)  # Using the pre-training RBM models as input
+    dae = DAE(rbm_models)  # Using the pre-training RBM models as input, add False as second parameter for Naive AE
 
     # optimizer = optim.SGD(dae.parameters(), 1e-3)
     optimizer = optim.Adam(dae.parameters(), 1e-3)  # dae.parameters() includes all the parameters for the dae list(dae.parameters())
